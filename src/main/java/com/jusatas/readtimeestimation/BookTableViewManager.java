14ca14148077c77b9b3aefe0b9d84e551baca9ef
Justas Drakšas <justas.draksas@mif.stud.vu.lt>
2024-07-23 14:50:29 +0300

    Added localization files, allowed users to change the programs langauge.

--- src/main/java/com/jusatas/readtimeestimation/BookTableViewManager.java
+++ src/main/java/com/jusatas/readtimeestimation/BookTableViewManager.java
@@ -4,37 +4,48 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class BookTableViewManager {
     private TableView<Book> tableView;
+    private ResourceBundle bundle;
 
-    public BookTableViewManager(TableView<Book> tableView) {
+    public BookTableViewManager(TableView<Book> tableView, Locale locale) {
         this.tableView = tableView;
+        this.bundle = ResourceBundle.getBundle("bundle", locale);
         initializeColumns();
     }
 
     private void initializeColumns() {
-        TableColumn<Book, String> nameColumn = new TableColumn<>("Name");
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn<Book, String> nameColumn = new TableColumn<>(bundle.getString("name"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        TableColumn<Book, Integer> wordCountColumn = new TableColumn<>("Word Count");
+        TableColumn<Book, Integer> wordCountColumn = new TableColumn<>(bundle.getString("wordCount"));
         wordCountColumn.setCellValueFactory(new PropertyValueFactory<>("wordCount"));
 
-        TableColumn<Book, Integer> pageCountColumn = new TableColumn<>("Page Count");
+        TableColumn<Book, Integer> pageCountColumn = new TableColumn<>(bundle.getString("pageCount"));
         pageCountColumn.setCellValueFactory(new PropertyValueFactory<>("pageCount"));
 
         tableView.getColumns().addAll(nameColumn, wordCountColumn, pageCountColumn);
     }
 
     public void addReadingTimeColumn() {
-        TableColumn<Book, Integer> readingTimeColumn = new TableColumn<>("Reading hours");
+        TableColumn<Book, Integer> readingTimeColumn = new TableColumn<>(bundle.getString("readingTime"));
         readingTimeColumn.setCellValueFactory(new PropertyValueFactory<>("readingTime"));
 
         tableView.getColumns().add(readingTimeColumn);
     }
 
     public void setItems(ObservableList<Book> items) {
-        System.out.println("Set items is being called");
         tableView.setItems(items);
+    }
+
+    public void updateLocale(Locale locale) {
+        this.bundle = ResourceBundle.getBundle("bundle", locale);
+        tableView.getColumns().clear();
+        initializeColumns();
     }
 }
--- src/main/java/com/jusatas/readtimeestimation/CalculatorController.java
+++ src/main/java/com/jusatas/readtimeestimation/CalculatorController.java
@@ -11,87 +11,122 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class CalculatorController {
 
     @FXML TextArea paragraphTextArea;
-    @FXML TextField bookName;
-    @FXML TextField pageCount;
-    @FXML TextField wordCount;
-
-    @FXML Button timerButton;
-    @FXML Button addBookButton;
-    @FXML Button calculateButton;
-
-    @FXML Label timerResultS;
-    @FXML Label wordsRead;
-    @FXML Label wordsPerMinute;
-
+    @FXML TextField bookName, wordCount, pageCount;
+    @FXML Button timerButton, addBookButton, calculateButton;
+    @FXML Label timerResultS, wordsRead, wordsPerMinute, timeSpentLabel, wordsReadLabel,
+            wpmLabel, bookNameLabel, pagecountLabel, wordcountLabel, infoLabel;
     @FXML TableView<Book> bookTableView;
+    @FXML ChoiceBox<String> languageChoiceBox;
 
     private ObservableList<Book> bookList;
     private boolean timerRunning = false;
     private String paragraphString;
-    private boolean timerClicked = false;
+    private TimerManager timerManager = TimerManager.getInstance();
+    private ParagraphManager paragraphManager;
 
-    private TimerManager timerManager = ManagerFactory.createTimerManager();
-    private ParagraphManager paragraphManager = ManagerFactory.createParagraphManger("paragraphs.txt");
     private BookTableViewManager bookTableViewManager;
+    private ResourceBundle bundle;
+    private Locale currentLocale = new Locale("en");
 
     public void initialize() {
-        bookTableViewManager = new BookTableViewManager(bookTableView);
+        languageChoiceBox.setItems(FXCollections.observableArrayList("English", "Lithuanian"));
+        languageChoiceBox.setValue("English");
+
+        Locale locale = new Locale("en");
+        bundle = ResourceBundle.getBundle("bundle", locale);
+        bookTableViewManager = new BookTableViewManager(bookTableView, locale);
         bookList = FXCollections.observableArrayList();
 
-        Book book = new Book("The Bible (ESV)", 757439, true);
-        bookList.add(book);
-        book = new Book("Metai", 20872, true);
-        bookList.add(book);
-        book = new Book("The Lord of the Rings series", 550147, true);
-        bookList.add(book);
+        addSampleBooks();
 
         bookTableViewManager.setItems(bookList);
+        paragraphManager = ParagraphManager.getInstance(locale);
+
+        languageChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            switch (newValue) {
+                case "English":
+                    updateLocale(new Locale("en"));
+                    break;
+                case "Lithuanian":
+                    updateLocale(new Locale("lt"));
+                    break;
+            }
+        });
+
+    }
+
+    private void addSampleBooks() {
+        bookList.addAll(
+                new Book("The Bible (ESV)", 757439, true),
+                new Book("Metai", 20872, true),
+                new Book("The Lord of the Rings series", 550147, true)
+        );
+    }
+
+    private void updateLocale(Locale locale) {
+        this.currentLocale = locale;
+
+        bundle = ResourceBundle.getBundle("bundle", locale);
+        bookTableViewManager.updateLocale(locale);
+
+        timerButton.setText(bundle.getString("startTimer"));
+        addBookButton.setText(bundle.getString("addBook"));
+        calculateButton.setText(bundle.getString("calculateResult"));
+        timeSpentLabel.setText(bundle.getString("timeSpent"));
+        wordsReadLabel.setText(bundle.getString("wordsRead"));
+        wpmLabel.setText(bundle.getString("wordsPerMinute"));
+        bookNameLabel.setText(bundle.getString("bookName"));
+        pagecountLabel.setText(bundle.getString("pageCount"));
+        wordcountLabel.setText(bundle.getString("wordCountOptional"));
+        infoLabel.setText(bundle.getString("info"));
+
+        paragraphManager.setLocale(locale);
 
     }
 
     @FXML
-    void onTimerButtonClick(ActionEvent event) {
+    void onTimerButtonClick() {
         if (timerRunning) {
             timerManager.stopTimer();
             timerRunning = false;
-            timerButton.setText("Start timer");
-
-            int wordCount = paragraphManager.countWords(paragraphString);
-            long timeS = timerManager.getElapsedTimeMS() / 1000;
-            if (timeS != 0) {
-                int wpm = (int)(wordCount / timeS * 60);
-                wordsPerMinute.setText(String.valueOf(wpm));
-            }
-            timerResultS.setText(String.valueOf(timeS));
-            wordsRead.setText(String.valueOf(wordCount));
-            timerClicked = true;
+            timerButton.setText(bundle.getString("startTimer"));
+            displayTimerResults();
         } else {
             timerManager.startTimer();
             timerRunning = true;
             paragraphString = paragraphManager.getRandomParagraph();
             paragraphTextArea.setText(paragraphString);
-
-            timerButton.setText("Stop timer");
+            timerButton.setText(bundle.getString("stopTimer"));
             timerResultS.setText("");
         }
     }
+
+    private void displayTimerResults() {
+        int wordCount = paragraphManager.countWords(paragraphString);
+        long timeS = timerManager.getElapsedTimeMS() / 1000;
+        if (timeS != 0) {
+            int wpm = (int) (wordCount / timeS * 60);
+            wordsPerMinute.setText(String.valueOf(wpm));
+        }
+        timerResultS.setText(String.valueOf(timeS));
+        wordsRead.setText(String.valueOf(wordCount));
+    }
+
     @FXML
-    void onAddBookButtonClick(ActionEvent event) {
+    void onAddBookButtonClick() {
         String name = bookName.getText();
         String pageCountText = pageCount.getText();
         String wordCountText = wordCount.getText();
         Book newBook;
 
         if (name.isEmpty() || (pageCountText.isEmpty() && wordCountText.isEmpty())) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText("Please fill in all the required fields");
-            alert.showAndWait();
+            showAlert(Alert.AlertType.ERROR, bundle.getString("error"), bundle.getString("ERROR_fieldsEmpty"));
             return;
 
         } else if (pageCountText.isEmpty()) {
@@ -110,28 +145,33 @@ public class CalculatorController {
     }
 
     @FXML
-    void onCalculateButtonClick(ActionEvent event) throws IOException {
-
-        if (!timerClicked) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText("Please read the text with the timer.");
-            alert.showAndWait();
+    void onCalculateButtonClick() throws IOException {
+        int readSpeed = 0;
+        try {
+            readSpeed = Integer.parseInt(wordsPerMinute.getText());
+        } catch (Exception e) {
+            showAlert(Alert.AlertType.ERROR, bundle.getString("error"), bundle.getString("ERROR_noTimer"));
             return;
         }
-        int readSpeed = Integer.parseInt(wordsPerMinute.getText());
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("timer-calculator-result-view.fxml"));
         Parent root = loader.load();
         ResultViewController resultViewController = loader.getController();
 
-        resultViewController.setData(readSpeed, bookList);
+        resultViewController.setData(readSpeed, bookList, currentLocale, bundle);
 
         resultViewController.initialize();
 
         Stage resultView = new Stage();
         resultView.setScene(new Scene(root));
         resultView.show();
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
